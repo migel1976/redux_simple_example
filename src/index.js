6d94f4b62b9fc2ad5574ea1b22c8778885a8d5b6
migel1976 <migel1976@gmail.com>
2020-02-06 14:53:32 +0300

    simple example work redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState={
+	name:'Mike',
+	secondName:'Petronv'
+};
+
+
+function reducer(state=initialState, action){
+	switch(action.type){
+		case 'CHANGE_NAME':
+			return{...state,name:action.payload};
+
+		case 'CHANGE_SECOND_NAME':
+			return{...state,secondName:action.payload};
+	}
+	return state;
+};
+
+const store=createStore(reducer);
+
+const changeName={
+	type:'CHANGE_NAME',
+	payload:'Ivan'
+};
+
+const changeSecondName={
+	type:'CHANGE_SECOND_NAME',
+	payload:'Ivanov'
+};
+
+console.log(store.getState());
+store.dispatch(changeName);
+console.log(store.getState());
+
+store.dispatch(changeSecondName);
+console.log(store.getState());
+
